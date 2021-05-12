ee8e55e2a7d9ceb44ce47a3c7563458eb21ec45b
Collin Chapman <cchap14@gmail.com>
2021-05-11 19:37:02 -0700

    create animal class in models
    Co-Authored-by: Salim Mayan <mailsalim@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,8 +1,15 @@
+// using System.Collections.Generic;
+using System;
+
 namespace AnimalShelter.Models
 {
   public class Animal
   {
-    // public int ItemId { get; set; }
-    // public string Description { get; set; }
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string DateOfAdmittance { get; set; }
+    public string Breed { get; set; }
   }
 }
